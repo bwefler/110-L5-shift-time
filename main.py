3ea1720109af747dba1f0e7dd8f651dbc6bf0bb4
bwefler <bruce.wefler@gmail.com>
2025-11-01 02:29:18 -0600

    Demonstrates date & time formats

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import datetime
 current_time = datetime.datetime.now()
 # print the month, day, and year in 00/00/00 format
 print(current_time.strftime("%m/%d/%y"))
-# print the month, day, and year in 00/00/0000 format
-print(current_time.strftime("%m/%d/%Y"))
+# print the month, day, and year in January 1, 2019 format
+print(current_time.strftime("%B %e, %Y"))
 # add the current time onto the date
-print(current_time.strftime("%m/%d/%Y %I:%M:%S %p"))
+print(current_time.strftime("%B %e, %Y %l:%M:%S %p"))
+
